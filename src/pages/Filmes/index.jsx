54363a049cd3c4c17bcbbe13ce6e6532d9862f04
Rafael-Notebook <rafaelso86@gmail.com>
2021-04-04 21:17:01 -0300

    Ajustes no header das listagems de Filmes por Categoria

--- src/pages/Filmes/index.jsx
+++ src/pages/Filmes/index.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 import { Link } from 'react-router-dom';
 
-import Header from '../../componentes/Header';
+import Helmet from 'react-helmet';
 import Menu from "../../componentes/Menu";
 
 import dateFormat from 'dateformat';
@@ -14,12 +14,14 @@ import { helperDataFormat } from '../../helpers/HelperDataFormat';
 
 import './index.scss';
 
-export default function FilmeLista() {
+export default function FilmeLista(props) {
     const { id } = useParams();
     //console.log(id);
 
     //const [categoria, setCategoria] = useState(id);
     const [lista, setLista] = useState([]);
+    const [categoria, setCategoria] = useState();
+    const [descricao, setDescricao] = useState();
 
     const listaFilmes = async () => {
         await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=c5ff834a7a048ff4e4c1e1610a68fb47&language=pt-BR&page=1`)
@@ -30,24 +32,47 @@ export default function FilmeLista() {
             })
     }
 
+    const categoriaTitulo = async () => {
+        if (id === 'popular') {
+            setCategoria('Filmes Populares | React Movies');
+            setDescricao('Confira todos os filmes mais populares.');
+        }
+        else if(id === 'now_playing') {
+            setCategoria('Filmes Em Cartaz | React Movies');
+            setDescricao('Confira todos os filmes em cartaz.');
+        }
+        else if(id === 'upcoming') {
+            setCategoria('Filmes Próximas Estréias | React Movies');
+            setDescricao('Confira todos os filmes em estréia.');
+        }
+        else if(id === 'top_rated') {
+            setCategoria('Filmes Mais Bem Avaliados | React Movies');
+            setDescricao('Confira todos os filmes mais bem avaliados.');
+        }
+        else {
+            setCategoria('Sem Categoria');
+        }
+    }
+
     useEffect(() => {
         listaFilmes();
         helperDataFormat();
+        categoriaTitulo();
     }, [id])
 
     return (
         <React.Fragment>
-                <Header
-                    title="Filmes Populares | React Movies"
-                    description="Filmes populares em React Movies"
-                    canonical="/filmes/populares"
-                />
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>{categoria}</title>
+                    <link rel="canonical" href={id} />
+                    <meta name="description" content={descricao}/>
+                </Helmet>
             
                 <Menu />
         
                 <div id="container">
                     <h1>Filmes Populares</h1>
-
                     <div className="card-group">
                         {lista.map(filme => <div className="cards">
                             <div className="card-body">
